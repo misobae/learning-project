11f18cb6c3af02fa67fb0491e114455280814ed3
misobae <misobae0731@gmail.com>
2024-02-10 12:35:23 +0900

    feat: 컴포넌트 분할

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Movie from './components/Movie';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -22,16 +23,13 @@ function App() {
         <h1>Loading...</h1> ) : (
         <div>
           {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((item, index) => (
-                  <li key={index}>{item}</li>
-                ))}
-              </ul>
-            </div>
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
           ))}
         </div>
       )}
