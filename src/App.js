40efa2ce4c7909e8518751197be649944d256938
misobae <misobae0731@gmail.com>
2024-02-10 14:42:49 +0900

    feat: React Router 페이지 네비게이션 구현

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
-import { useEffect, useState } from 'react';
-import Movie from './components/Movie';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
-    const json = await (
-      await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  }
-  useEffect(() => {
-    getMovies();
-  }, [])
-  console.log(movies);
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1> ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/movie/:id" element={ <Detail /> } />
+      </Routes>
+    </Router>
   );
 }
 
