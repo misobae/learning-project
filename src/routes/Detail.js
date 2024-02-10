f3fed30077c545afe569d69ad99c5d4b97568c14
misobae <misobae0731@gmail.com>
2024-02-10 15:14:45 +0900

    feat: 영화 상세정보 페이지 컴포넌트 분할

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import MovieDetails from "../components/MovieDetails";
 
 function Detail(){
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
   }
   useEffect(() => {
     getMovie();
   }, [])
   
-  return <h1>Detail</h1>
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <MovieDetails
+      coverImg={movie.medium_cover_image}
+      title={movie.title}
+      rating={movie.rating}
+      runtime={movie.runtime}
+      genres={movie.genres}
+      description={movie.description_full}
+    />
+  )
 }
 export default Detail;
