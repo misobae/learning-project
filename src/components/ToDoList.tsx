6edcd0f2c7a9a0b23ebf74b9f4f3518607c807b2
misobae <misobae0731@gmail.com>
2024-02-20 21:27:42 +0900

    feat: category 추가 기능 구현

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,27 +1,62 @@
 import { useRecoilState, useRecoilValue } from "recoil";
-import { Categories, categoryState, toDoSelector, toDoState } from "./atoms";
+import { Categories, categoryState, toDoSelector, catsState } from "./atoms";
+import { useForm } from "react-hook-form";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
+interface IForm {
+  categoryName: string;
+}
 
 function ToDoList(){
   // useRecoilState는 value와 modifier 함수를 반환. setState와 비슷
   // value만 얻고 싶을 땐 useRecoilValue를, value를 바꾸고만 싶을 때는 useSetRecoilState 사용
   const toDos = useRecoilValue(toDoSelector);
   const [category, setCategory] = useRecoilState(categoryState);
-  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+  const [customCats, setCustomCats] = useRecoilState(catsState);
+  const { handleSubmit, register, setValue } = useForm<IForm>();
+
+  const handleChangeCat = (event: React.FormEvent<HTMLSelectElement>) => {
     setCategory(event.currentTarget.value as any);
   };
+
+	const handleAddCat = ({ categoryName }: IForm) => {
+    setCustomCats((oldCats) => [
+      ...oldCats,
+      {
+        text: categoryName,
+        id: Date.now()
+      },
+    ]);
+    setCategory(categoryName as any);
+		setValue("categoryName", "");
+	};
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-      <select value={category} onChange={onInput}>
-        <option value={Categories.TO_DO}>To Do</option>
-        <option value={Categories.DOING}>Doing</option>
-        <option value={Categories.DONE}>Done</option>
-      </select>
+      <form onSubmit={handleSubmit(handleAddCat)}>
+        <select value={category} onChange={handleChangeCat}>
+          <option value={Categories.TO_DO}>To Do</option>
+          <option value={Categories.DOING}>Doing</option>
+          <option value={Categories.DONE}>Done</option>
+          {customCats.map((customCat) => (
+            <option key={customCat.id} value={customCat.text}>
+              {customCat.text}
+            </option>
+          ))}
+        </select>
+
+        <input
+          {...register("categoryName", {
+            required: "Please, write a category!",
+          })}
+          placeholder="Write a category."
+        />
+        <button name="newCat">NEW</button>
+      </form>
+
       <CreateToDo />
       {toDos?.map((toDo) => (
         <ToDo key={toDo.id} {...toDo} />
--- src/components/atoms.tsx
+++ src/components/atoms.tsx
@@ -1,15 +1,11 @@
 import { atom, selector } from "recoil";
 
-export const enum Categories { // enum은 열거형으로 이름이 있는 상수들의 집합을 정의하는 기능
+export enum Categories {
+  // enum은 열거형으로 이름이 있는 상수들의 집합을 정의하는 기능
+  // TypeScript에서 열거형은 객체가 아니라 "리터럴 타입"으로 처리됨
   "TO_DO" = "TO_DO",
   "DOING" = "DOING",
   "DONE" = "DONE",
-}
-
-export interface IToDo {
-  text: string;
-  id: number;
-  category: Categories;
 }
 
 export const categoryState = atom<Categories>({
@@ -17,9 +13,23 @@ export const categoryState = atom<Categories>({
   default: Categories.TO_DO,
 });
 
+export interface IToDo {
+  text: string;
+  id: number;
+  category: Categories;
+}
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: []
+})
+
+export interface ICategory {
+	text: string;
+	id: number;
+}
+export const catsState = atom<ICategory[]>({
+  key: "categories",
+	default: [],
 })
 
 export const toDoSelector = selector({
