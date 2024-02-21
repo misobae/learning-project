516cdf753e1cd44f6ae815d10cc838eaefac50d4
misobae <misobae0731@gmail.com>
2024-02-21 23:24:23 +0900

    feat: to do input 스타일 수정

--- src/components/CreateToDo.tsx
+++ src/components/CreateToDo.tsx
@@ -1,7 +1,33 @@
 import { useForm } from "react-hook-form";
 import { useSetRecoilState, useRecoilValue } from "recoil";
 import { categoryState, toDoState } from "./atoms";
+import styled from "styled-components";
 
+const AddBox = styled.form`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+  margin-top: 16px;
+  border: 1px solid #333;
+  border-radius: 16px;
+  padding: 6px 12px;
+`;
+const AddInput = styled.input`
+  display: block;
+  width: 100%;
+  border: 0;
+  font-size: 16px;
+  &:focus {
+    outline: none;
+  }
+`;
+const AddBtn = styled.button`
+  border: 0;
+  background: transparent;
+  font-size: 22px;
+  cursor: pointer;
+`;
 interface IForm {
   toDo: string;
 }
@@ -10,7 +36,7 @@ function CreateToDo(){
   const setToDos = useSetRecoilState(toDoState);
   const category = useRecoilValue(categoryState);
   const { register, handleSubmit, setValue } = useForm<IForm>();
-  const handleValid = ({toDo}: IForm) => { // Destructuring: data.toDo -> {toDO}
+  const handleValid = ({ toDo }: IForm) => { // Destructuring: data.toDo -> {toDO}
     setToDos(oldToDos => [{
       text: toDo,
       id: Date.now(),
@@ -19,12 +45,12 @@ function CreateToDo(){
   }
 
   return (
-    <form onSubmit={handleSubmit(handleValid)}>
-      <input {...register("toDo", {
-        required: "Please write a todo"
-      })} placeholder="Write somthing" />
-      <button>Add</button>
-    </form>
+    <AddBox onSubmit={handleSubmit(handleValid)}>
+      <AddInput {...register("toDo", {
+        required: "Please write a todo",
+      })} placeholder="Click here and write somthing to complete!" />
+      <AddBtn>+</AddBtn>
+    </AddBox>
   )
 }
 
