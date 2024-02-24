6071c439ba8fb0ff1f37479055fb770c57447cf2
misobae <misobae0731@gmail.com>
2024-02-24 17:14:52 +0900

    feat: Header Component 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './routes/Home';
+import Tv from './routes/Tv';
+import Search from './routes/Search';
+import Header from './components/Header';
+
+
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
