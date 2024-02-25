9b30fc6c7df39da8737c535ee0e0c55f4ba5203b
misobae <misobae0731@gmail.com>
2024-02-25 22:22:20 +0900

    feat: Tv Page에 Slider 컴포넌트 추가

--- src/routes/Tv.tsx
+++ src/routes/Tv.tsx
@@ -1,5 +1,39 @@
+import { useQuery } from "react-query";
+import styled from "styled-components";
+import { IGetMoviesResult, getTvSeries } from "../api/movieApi";
+import Slider from "../components/Slider";
+
+const Wrap = styled.div`
+  margin-top: 100px;
+`;
+const Loader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20vh;
+`;
+
 function Tv(){
-  return <h1>Tv</h1>
+  const { data: tvAiringToday, isLoading } = useQuery<IGetMoviesResult>(["tv", "airing_today"], () => getTvSeries("airing_today"));
+  const { data: tvOnTheAir } = useQuery<IGetMoviesResult>(["tv", "on_the_air"], () => getTvSeries("on_the_air"));
+  const { data: tvPopular } = useQuery<IGetMoviesResult>(["tv", "popular"], () => getTvSeries("popular"));
+  const { data: tvTopRated } = useQuery<IGetMoviesResult>(["tv", "top_rated"], () => getTvSeries("top_rated"));
+
+  return (
+    <>
+    {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <Wrap>
+          <Slider data={tvAiringToday || null} category="airing_today" title="Airing Today" />
+          <Slider data={tvOnTheAir || null} category="on_the_air" title="On The Air" />
+          <Slider data={tvPopular || null} category="popular" title="Popular" />
+          <Slider data={tvTopRated || null} category="top_rated" title="Top Rated" />
+        </Wrap>
+      )
+    }
+    </>
+  )
 }
 
 export default Tv;
